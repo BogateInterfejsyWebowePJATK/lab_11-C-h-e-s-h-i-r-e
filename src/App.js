ae6e77a25bc613476527b71610d6b863b9d8ba91
C-h-e-s-h-i-r-e <aleksanderbober1999@gmail.com>
2021-01-11 09:03:58 +0100

    zad1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+//import data from "./data/recipes.json";
+//import Rand from "./components/random";
 
 function App() {
+
+var min = 0;
+var max = 10;
+   
+
+var list = [];
+
+for(var i = 0; i < 10 ; i++){
+var rand =  min + (Math.random() * (max-min));
+list[i] = Math.floor(rand);
+
+}; 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+      <ul className="rand_list">
+            {list.map((random, i) => (
+                <li id={i}>{random}</li>
+            ))}
+        </ul>
+
   );
 }
 
